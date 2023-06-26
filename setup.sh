c51ee1be65f5ec77a1a61e046ce583e0c57e80b0
Hasanudin29 <135009835+Hasanudin29@users.noreply.github.com>
2023-06-26 15:41:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -66,38 +66,38 @@ clear
 echo '============================================='
 echo '        Installing SSH & OPENVPN '
 echo '============================================='
-wget https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/Hasanudin29/CtechDidikAutoScriptVPN/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 echo '============================================='
 echo '        Installing SSTP '
 echo '============================================='
-wget https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/Hasanudin29/CtechDidikAutoScriptVPN/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 echo '============================================='
 echo '        Installing ssr '
 echo '============================================='
-wget https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/Hasanudin29/CtechDidikAutoScriptVPN/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
 echo '============================================='
 echo '        Installing shadowsocksobfs '
 echo '============================================='
-wget https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://raw.githubusercontent.com/Hasanudin29/CtechDidikAutoScriptVPN/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 echo '============================================='
 echo '        Installing WIREGUARD '
 echo '============================================='
-wget https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/Hasanudin29/CtechDidikAutoScriptVPN/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 echo '============================================='
 echo '        Installing V2RAY '
 echo '============================================='
-wget https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/Hasanudin29/CtechDidikAutoScriptVPN/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 echo '============================================='
 echo '        Installing ipsec '
 echo '============================================='
-wget https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/Hasanudin29/CtechDidikAutoScriptVPN/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 echo '        Installing XRAY '
 echo '============================================='
-wget https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/install-xray.sh && chmod +x install-xray.sh && screen -S v2ray ./install-xray.sh
+wget https://raw.githubusercontent.com/Hasanudin29/CtechDidikAutoScriptVPN/main/install-xray.sh && chmod +x install-xray.sh && screen -S v2ray ./install-xray.sh
 echo '============================================='
 echo '        Installing Websocket '
 echo '============================================='
-wget https://raw.githubusercontent.com/CTechDidik/CtechDidikAutoScriptVPN/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/Hasanudin29/CtechDidikAutoScriptVPN/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 
 
 rm -f /root/ssh-vpn.sh
